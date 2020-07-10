b75b278e0e496dbbbb3f8497e57aba4e8a441700
rohangupta <rohaninjmu@gmail.com>
2020-07-10 16:10:08 +0530

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by pamarori mac on 10/07/20.
+//  Copyright © 2020 pamarori mac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
